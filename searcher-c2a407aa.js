e16ef357766e635c9f43dd66ee9459be4601351b
yomaq <yomaq@users.noreply.github.com>
2026-05-07 01:09:00 +0000

    deploy: d601243446132e72caaf35180a2048ad28bb15c4

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-039d766e.js',
+                path_to_root + 'searchindex-17be2b77.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
